6a2bc28e1c630e764343f52d9979031ebb35ec64
Pasqat <pasquale.matarrese@gmail.com>
2020-06-10 16:31:08 +0200

    update shop data with new date, update cart utils to group items in cart reducer

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -2,6 +2,10 @@ import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
-
+});
 //no pyaload perchè cambiamo solo lo state da true a false nel reducer
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
