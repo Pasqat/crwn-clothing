53e182d547f5d943d44b7337f576cba77534750f
Pasqat <pasquale.matarrese@gmail.com>
2020-06-10 10:41:14 +0200

    updating app to redirec to home is user sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
@@ -16,8 +16,7 @@ class App extends React.Component {
   unsubrisbedFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props
-
+    const { setCurrentUser } = this.props;
 
     this.unsubrisbedFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -25,10 +24,10 @@ class App extends React.Component {
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
+            id: snapShot.id,
+            ...snapShot.data()
           });
+        });
       } else {
         setCurrentUser(userAuth); //dove userAuth è null
       }
@@ -46,15 +45,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/user/user.types.js
@@ -0,0 +1,5 @@
+export const userActionTypes = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+//ci serve come costante ed evitare di modificarlo per sbaglio in user.action e user.reducer
